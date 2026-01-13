7724c5a9f09fcc14a7db49d32471352f8a812cee
GitCodersFA <i222138@nu.edu.pk>
2026-01-13 15:20:51 +0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# Models package
-from .attention import SpatialAttention
-from .feature_extractor import FeatureExtractor, DefectClassifier
-
-__all__ = ['SpatialAttention', 'FeatureExtractor', 'DefectClassifier']
